c6676dbe61280100aa5fb18183a8b508bd15ba17
sebitamontana <93792473+sebitamontana@users.noreply.github.com>
2022-01-27 00:08:06 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -185,7 +185,7 @@ class MainMenuState extends MusicBeatState
 			{
 				if (optionShit[curSelected] == 'donate')
 				{
-					CoolUtil.browserLoad('https://ninja-muffin24.itch.io/funkin');
+					CoolUtil.browserLoad('https://youtube.com/channel/UCmdcI4Ma8ZWDnG10QWlUv8g');
 				}
 				else
 				{
